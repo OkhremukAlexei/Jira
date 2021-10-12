9d86e2b2c3c9219a443be6d564e6c14d0bd10d5b
Leha <ale9999@tut.by>
2021-10-12 20:33:13 +0300

    added any fields to models

--- src/main/java/com/jira/models/Project.java
+++ src/main/java/com/jira/models/Project.java
@@ -1,4 +1,22 @@
 package com.jira.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String linkToGit;
+    private int progress;
+
+
+//    private Status status; ?
+//    private Team team; ?
 }
--- src/main/java/com/jira/models/Task.java
+++ src/main/java/com/jira/models/Task.java
@@ -1,4 +1,20 @@
 package com.jira.models;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String title;
+    private String description;
+    private LocalDateTime dateTime;
+    private int spentTime;
+
+   /* @OneToMany   ?
+    private Comment comment;*/
 }
