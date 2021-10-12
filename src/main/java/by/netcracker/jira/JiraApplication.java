18ac88fc243c072e8c77da793734327df3dffb42
Alexander <okolotovich2000@mail.ru>
2021-10-12 21:22:08 +0300

    add model user and role

--- src/main/java/by/netcracker/jira/JiraApplication.java
+++ src/main/java/by/netcracker/jira/JiraApplication.java
@@ -1,4 +1,4 @@
-package com.jira;
+package by.netcracker.jira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/by/netcracker/jira/model/Role.java
@@ -0,0 +1,16 @@
+package by.netcracker.jira.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
