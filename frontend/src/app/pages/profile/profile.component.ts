e10a22d8b4eb0da7b93854fccf20b01a504719ec
Rozanov <rozanov_danila@mail.ru>
2022-01-18 16:32:31 +0300

    RespionseEntity #1

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -14,51 +14,29 @@ import {Observable, of} from "rxjs";
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
 
     public id:string;
     public user$:Observable<User>;
-    public tasks$:Observable<TasksInfo[]>;
-    public projects$:Observable<Projects[]>;
-  public user:User;
-  public projects:Projects[];
+    public user:User;
 
     public ngOnInit() {
-       }
+
+    }
 
 
-    public constructor(private tokenStorageService:TokenStorageService, private userService:UserService, private projectService:ProjectsService, private tasksService:TasksService) {
+    public constructor(private tokenStorageService:TokenStorageService, private userService:UserService) {
       this.id=this.tokenStorageService.getId();
       console.log(this.id);
 
-      this.user$ = of(new User(
-     1337,
-        "testLogin",
-         "testPassword",
-         "TEST_ROLE",
-        new Account (
-          "Test",
-          "Surname",
-           "testEmail@gmail.com"
 
-      )
-      ))
 
-      // this.user$ = this.userService.getUser(this.id);
+      this.user$ = this.userService.getUser(this.id);
         this.user$.subscribe(user => {
           this.user = user
           console.log(user)
-        })
-
-      // this.projects$ = this.projectService.getProjectsFromUserId(this.id);
-
-      this.projects$ = of([new Projects(123, "test project", "url.com", 2, 5, [this.user], this.user),
-      new Projects(321, "another test project", "url2.com", 4, 1, [this.user], this.user)])
-
-      this.projects$.subscribe(projects => {
-        this.projects = projects
-      })
-
-        // this.tasks$ = this.tasksService.getAllTasksByUserId(this.id)
+        }
+     )
     }
 }
