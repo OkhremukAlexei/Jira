a1dee48509791649c95e9650d7cdffc8da3bada2
Rozanov <rozanov_danila@mail.ru>
2022-01-24 20:30:57 +0300

    Fix

--- backend/src/main/java/com/jira/controllers/AdminController.java
+++ backend/src/main/java/com/jira/controllers/AdminController.java
@@ -8,6 +8,7 @@ import com.jira.repos.UserRepo;
 import com.jira.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -34,19 +35,24 @@ public class AdminController {
 
     @GetMapping("/userlist")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> getAllUsers() {
-        return ResponseEntity.ok(adminService.findAll());
+    public ResponseEntity<List> getAllUsers()
+    {
+        List users = adminService.findAll();
+        return users != null &&  !users.isEmpty()
+                ? new ResponseEntity<>(users, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/userinfo/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getUser(@PathVariable("id") Long id) {
-        if (userRepo.existsById(id)) {
+        /*if (userRepo.existsById(id)) {
             return ResponseEntity.ok(userRepo.findById(id));
         }
         else
             return ResponseEntity.badRequest().
-                    body(new MessageResponse("Error: User with this id is not exist "));
+                    body(new MessageResponse("Error: User with this id is not exist "));*/
+        return ResponseEntity.ok(new MessageResponse("Get User"));
     }
 
     @PostMapping("/manager/{id}")
